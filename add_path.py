4670424d9f61fd589a103d4df304a25bda064e9c
Wouter van Ooijen <wouter@voti.nl>
2021-01-03 11:36:42 +0100

    update

--- add_path.py
+++ add_path.py
@@ -5,5 +5,5 @@ env = "/etc/environment"
 line = open( env ).readlines()[ 0 ]
 for x in sys.argv[ 1 : ]:
    if line.find( x ) < 0:
-      line = line.replace( '="', '="' + x + ':' )
+      line = line.replace( 'PATH="', 'PATH="' + x + ':' )
 open( env, "w" ).write( line )
